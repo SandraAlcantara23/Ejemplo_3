88622ade7b1f440e02694b822c195c7287007430
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2024-03-09 22:55:24 -0600

    Add files via upload

---
+++ pract_4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a = 6;
+    int b = 7;
+    int multiplicacion = a * b;
+    printf("El producto de %d y %d es %d\n", a, b, multiplicacion);
+    return 0;
+}
